3385e1b096c80f2b3fa00d6c9d77f54295a56b8a
Henrique Risciere <henriquerisc@gmail.com>
2026-01-03 15:12:32 -0300

    video07: RequestMapping and Servlet Context

--- anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+ // @RequestMapping(value = "greetings")
 public class HelloController {
+
+    // Posso usar o RequestMapping a nível de classe e colocar o /greetings em todos os endpoints da classe
 
     @GetMapping("hi")
     public String hi() {
@@ -17,4 +20,14 @@ public class HelloController {
 //    }  -- No dia a dia é usado um "reverse proxy" para normalizar, então não é bom fazer isso
 //    O outro problema é que precisa fazer a proteção dos dois endpoints, tanto com "hi" quanto com "hi/"
 //    Mesmo indo para o mesmo lugar o spring identifica eles como sendo DIFERENTES, então eu vou deixar apenas o "hi"
+
+//    @RequestMapping(method = RequestMethod.GET, value = "hi")
+//    public String hi() {
+//        return "OMAE WA MOU SHINDE IRU";
+//    }   -- Forma antiga de usar o @GetMapping
+
+//    @GetMapping("greetings/hi")
+//    public String hi() {
+//        return "OMAE WA MOU SHINDE IRU";
+//    } -- Posso fazer assim, se eu quiser
 }
